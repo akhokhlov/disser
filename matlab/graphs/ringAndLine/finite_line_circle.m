9f247565ccc574cf8e229c9a3f591a8be1e9053f
user <user@user-PC>
2012-11-06 22:04:18 +0600

    fix error

--- matlab/graphs/ringAndLine/finite_line_circle.m
+++ matlab/graphs/ringAndLine/finite_line_circle.m
@@ -13,14 +13,20 @@ if large
     fileExt = '_large';
 end
     
-
-createFigure(.5, 3);
-createFigure(.5, 4);
-createFigure(.5, 5);
+% createFigure(.5, 3);
+% createFigure(.5, 4);
+% createFigure(.5, 5);
 createFigure(.5, 5, [-0 1 -0 1]);
-createFigure(.5, 6);
-createFigure(.5, 7);
+% createFigure(.5, 6);
+% createFigure(.5, 7);
+
+for k=3:16
+    createFigure(.5, k);
+end
+
 close all;
+
+
 
 
     function createFigure(tau, n, zoom)
@@ -55,32 +61,34 @@ close all;
 
         set(gca, 'XTick',[ -2 -1 0 1 2], 'YTick',[-2 -1 0 1 2], 'FontSize',ticksize);
   
-        filename = [mfilename '_n' num2str(n) fileExt '.eps'];
+        
+        fileFormat = 'eps';
+        filename = [mfilename '_n' num2str(n) fileExt '.' fileFormat];
         axis([-bound bound -bound bound]);
         if (exist('zoom', 'var'))
             axis(zoom);
             % Дополнительные границы
             plot(zoom(1:2), [ zoom(2) zoom(2)], 'k', 'LineWidth', 1);
             plot([ zoom(end) zoom(end)], zoom(3:end), 'k', 'LineWidth', 1);
-            filename = [mfilename '_n' num2str(n) '_zoom' fileExt '.eps'];
+            filename = [mfilename '_n' num2str(n) '_zoom' fileExt '.' fileFormat];
         end
 
       
         set(figure1, 'PaperPositionMode', 'auto');
-        saveas(figure1, filename, 'psc2');
+        saveas(figure1, filename, fileFormat);
 
     end
 
-    
-    function minW = minW(tau)
+    function minW = minOmega(tau)
+        % функция f = w*tg(w) пересекается в одной точке с f = x
+        % и лежит под ней около нуля.
         w = 0: 0.01: pi/2;
         func = w.*tan(w);
-        
         ind = 1;
         while func(ind) < tau
             ind = ind + 1;
         end
-        minW = func(ind);
+        minW = w(ind);
     end
 
     function FF = Fodd(n)
@@ -88,14 +96,14 @@ close all;
     end
 
     function FF = Feven(tau, n)
-        w = minW(tau);
-        FF = 1/(4*sin(w).^2 * cos(pi/(n+1)).^2); 
+        w = minOmega(tau);
+        FF = 1/(4*(sin(w)).^2 * cos(pi/(n+1)).^2); 
     end
 
  function plot_gip(tau, n, bound)
         x = -bound:.01:bound;
-        plot(x, Fodd(n)./x, 'color', 'k', 'LineWidth', 3);
-        plot(x, -Feven(tau, n)./x, 'color', 'k', 'LineWidth', 3);
+        plot(x, Fodd(n)./x, 'color', 'k', 'LineWidth', 2);
+        plot(x, -Feven(tau, n)./x, 'color', 'k', 'LineWidth', 2);
  end
 
 end
