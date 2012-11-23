388a36732402a438f0177a7f385d874827617aea
user <user@user-PC>
2012-11-23 23:59:49 +0600

    img

--- matlab/graphs/breakingRing/breakingRing.m
+++ matlab/graphs/breakingRing/breakingRing.m
@@ -5,16 +5,16 @@ function breakingRing
     type = 2;
     tau = 0.1;
     number = 6;
-    epsilon = 0.15;   
+    epsilon = 0.1;   
     
-     plotterBoundness(type, tau, number, epsilon, 1);
-     plotterBoundness(type, tau, number, epsilon, 0.5);
-     plotterBoundness(type, tau, number, epsilon, 0.1);
-     plotterBoundness(type, tau, number, epsilon, 0.01);
-     plotterBoundness(type, tau, number, epsilon, 0.001);
-     plotterBoundness(type, tau, number, epsilon, 0.0001);
+      plotterBoundness(type, tau, number, epsilon, 1);
+       plotterBoundness(type, tau, number, epsilon, 0.5);
+       plotterBoundness(type, tau, number, epsilon, 0.1);
+       plotterBoundness(type, tau, number, epsilon, 0.01);
+%      plotterBoundness(type, tau, number, epsilon, 0.001);
+      plotterBoundness(type, tau, number, epsilon, 0.0001);
 
-     close all;
+    close all;
    
     
      
